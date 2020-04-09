9c00ae82c4cf4f50d64ca369e99448d616d8f883
Austin Gardner <afg@uoregon.edu>
2020-04-09 15:49:03 -0700

    Added arrAvg to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -4,15 +4,23 @@
 
 let arrAvg = (arr) => {
   let sum = 0;
+  let length = arr.length;
+  let average = 0;
 
   for (var i = 0; i < arr.length; i++) {
-    //arr[i]
+    sum = sum + arr[i];
   }
+
+  average = sum / length;
+
+  console.log(average);
+  return average;
+
 };
 
 //Req 3c
 
-//From lecture ex, double check it works
+//From lecture ex, double check it works (IT DOES NOT)
 let arrMax = (arr) => {
   let maxNow = arr[0];
   let results = "";
